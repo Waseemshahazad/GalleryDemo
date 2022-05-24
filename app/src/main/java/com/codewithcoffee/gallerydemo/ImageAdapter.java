d52da33ef4411b40bce7c216a8f324ab6582f7a2
androiddevelper <waseemshahzad226@gmail.com>
2022-05-24 10:15:07 +0500

    Initial commit

---
+++ app/src/main/java/com/codewithcoffee/gallerydemo/ImageAdapter.java
@@ -0,0 +1,65 @@
+package com.codewithcoffee.gallerydemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.viewHolder> {
+    private ArrayList<imageModel> list;
+    private Context context;
+
+    public ImageAdapter(ArrayList<imageModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(
+                R.layout.list_items, parent, false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        Glide.with(context).load(
+                list.get(position).getPath()).into(holder.imageView);
+
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String parseData = list.get(position).getPath().toString();
+                context.startActivity(new Intent(context,FullScreenActivity.class)
+                        .putExtra("parseData", parseData));
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView imageView;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/codewithcoffee/gallerydemo/MainActivity.java
@@ -0,0 +1,101 @@
+package com.codewithcoffee.gallerydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<imageModel>list;
+    ImageAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        list = new ArrayList<>();
+
+        GridLayoutManager layoutManager = new
+                GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new ImageAdapter(list,this);
+        recyclerView.setAdapter(adapter);
+
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        ReadSdcard(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+
+    }
+    private void ReadSdcard(Context context){
+        Uri collection;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL);
+        }else
+        {
+            collection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        }
+        String projection[] = new String[]{
+                MediaStore.Images.Media._ID
+        };
+
+        try(Cursor cursor = MainActivity.this.getContentResolver().query(
+                collection,
+                projection,
+                null,
+                null
+        )){
+            int idColumn = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+            while (cursor.moveToNext()){
+                long id = cursor.getLong(idColumn);
+                Uri contentUri = ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id);
+                list.add(new imageModel(contentUri));
+            }
+            adapter.notifyDataSetChanged();
+            cursor.close();
+
+        }
+
+
+    }
+}
